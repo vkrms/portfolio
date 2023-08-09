1f0bfaebce86ed6c52b9ca6059de48ddcd64099f
Pavel M <april504@yandex.ru>
2023-08-09 17:57:15 +0700

    feat: add dowell project

---
+++ data/posts/dowell.js
@@ -0,0 +1,53 @@
+const job = {
+    client: 'Dowell.team',
+
+    title: "A Lively Vanilla JS landing page",
+
+    images: [
+        '/screens/dowell/a.webp',
+        '/screens/dowell/b.webp',
+        '/screens/dowell/c.webp',
+        '/screens/dowell/d.webp',
+    ],
+
+    text: `
+            I landed a job to build this lively, bold thing -- an animated landing page for an IT consulting agency. Sadly the company didn't take off.
+
+            I went with vanilla js & pug for repeating elements cause I decided you don't have to cram a SPA framework everywhere, right? )
+
+            It has a growing circle, which paints over the background, lots of color-changing SVGs, an accordion, .gifs which are actually .webms to save space... Lots of stuff. You better check out the video.
+
+            Besides all the animations, it also had an express.js server for collecting leads into Telegram, instead of usual emails.
+    `,
+
+    excerpt: `for an IT consulting agency. With dynamic SVGs, background animations, accordions, and .webm visuals`,
+
+    // links: [
+    //     {
+    //         text: "Here's what it looked like in action",
+    //         url: "https://watch.screencastify.com/v/U7fTr2IVox8NZ6Rs6tvd"
+    //     }
+    // ],
+
+    roles: [
+        'FRONTEND ENGINEER',
+        'FULLSTACK ENGINEER',
+        'WEB DEVELOPER',
+    ],
+
+    tools: [
+        'FIREBASE',
+        'JAVASCRIPT',
+        'VUE.JS',
+    ],
+
+    thumb: `https://media.contra.com/image/upload/ar_1.333,c_fill,f_auto,q_auto,w_384/titnde36nyiuoypugm2t`,
+
+    years: 'Spring 2022',
+
+    videos: [
+        '/videos/dowell.webm'
+    ],
+}
+
+export default job
