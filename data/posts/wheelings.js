179151beb1d78b99ed127d69ad061720ae4c4904
Pavel M <april504@yandex.ru>
2023-08-16 18:18:55 +0700

    feat(content): add wheelings

---
+++ data/posts/wheelings.js
@@ -0,0 +1,42 @@
+const job = {
+    created: '2023-08-16',
+
+    client: 'Myself',
+
+    title: "Rotating Feelings Wheel in React & d3.js",
+
+    images: [
+        ''
+    ],
+
+    text: `
+        %%
+    `,
+
+    excerpt: `%%`,
+
+    links: [
+        {
+            text: "Here's what it looks like in action",
+            url: "https://wheelings.vercel.app/"
+        }
+    ],
+
+    roles: [
+        'FRONTEND ENGINEER',
+        'FULLSTACK ENGINEER',
+        'WEB DEVELOPER',
+    ],
+
+    tools: [
+        'd3.js',
+        'JAVASCRIPT',
+        'REACT',
+        'vite',
+        'vercel'
+    ],
+
+    thumb: `/screens/wheelings/thumb.webp`,
+}
+
+export default job
