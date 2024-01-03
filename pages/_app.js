00b55ce99e658d029a9b5bbb4b3409288bf17351
Pavel M <april504@yandex.ru>
2024-01-03 19:07:29 +0700

    wip: add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.scss'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -6,6 +7,18 @@ function MyApp({ Component, pageProps }) {
       {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200..700&display=swap" rel="stylesheet" /> */}
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-FVEQZ41RC6" />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FVEQZ41RC6');`}
+      </Script>
+
+      {/* everything else */}
       <Component {...pageProps} />
     </>
   )
